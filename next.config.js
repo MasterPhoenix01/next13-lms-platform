d1c0ac8aa956c555b86e2103eb5d22686c4f2e4a
MasterPhoenix01 <masterxt10@gmail.com>
2023-11-14 17:29:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,    // Enable Server Actions
+},
   images: {
     domains: [
       "utfs.io"
